cc01a48f6b201250eb68f9926a5556d47fc2b753
Lucas <lucasgabrielcamposoliveira@gmail.com>
2024-04-25 15:02:21 -0300

    ''

--- Dia 6/Index.js
+++ Dia 6/Index.js
@@ -1,33 +1,23 @@
-let escolha = Number(prompt('Digite 1 para o exercicio 1, 2 para o exercicio 2 e 3 para o exercicio 3'));
-//Utilizando o Switch Case para o usuário navegar entre os exercicios
-switch(escolha){
-    case 1 :
-        let numero1 = parseInt(prompt('Digite seu número'));
-        for(contadorCase1=0; contadorCase1 <= numero1; contadorCase1++){
-            console.log('Exibindo contagem de 0 até o Número desejado');
-            console.log(contadorCase1); 
-        }
-        break;
-    case 2 :
-        for (contadorCase2 = 0; contadorCase2<55;contadorCase2+=5){
-            console.log(contadorCase2);  
-        }
-        break;
-    case 3 :
-        for (contadorCase3 = 50;contadorCase3 >=0 ;contadorCase3-=5){
-            console.log(contadorCase3)
-        }
-        break;
-    case 4:
-        let numero2 = parseInt(prompt('Digite um número'))
-        for(let i = numero2;i<= numero2 + 2;i++){//ENQUANTO I FOR MENOR QUE O NUMERO DO USUARIO + 2, INCREMENTA + 1 Á VARIAVEL I
-            console.log('Tabuada do número: '+i)
-            for (let contadorCase4 = 0;contadorCase4<=10;contadorCase4++){//ENQUANTO O CONTADOR CASE 4 FOR MENOR QUE 10, INCREMENTA 1 A VARIAVEL
-                console.log(i+ ' X '+ contadorCase4+' = '+(i*contadorCase4)) //I X CONTADOR CASE 4 = I *CONTADOR CASE 4
-            }
-        }
-        break;
-    default :
-        console.log('Digite um Número de 1 a 4')
-        break;
+let EscolhaUsuario = parseInt(prompt('Digite de 1 a 3 para escolher qual programa executar'))
+switch(EscolhaUsuario){
+   case 1: 
+     let programa1 = parseInt('Digite o numero desejado');
+      for(let contador = 0;contador <= programa1; contador ++ ){
+         console.log('Exibindo contagem de 0 até o Número recebido')
+         console.log(contador)
+      }
+   break;
+   case 2:
+      let numero5em5 = 50
+         for(let contador = 0;contador <= numero5em5; contador += 5 ){
+            console.log('Exibindo contagem de 0 a 50 de 5 em 5');
+      }
+   break;
+   case 3: 
+   let numero5em5Invertido = 0
+      for(let contador = 50; contador == numero5em5Invertido; contador-=5){
+         console.log('Exibindo contagem Invertida de 50 a 0 de 5 em 5')
+         console.log(contador)
+      }
+   break;
 }
